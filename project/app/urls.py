c4ef8d4426b34223f9cb423415851decd36dc175
raziqpv333 <raziqpv333@gmail.com>
2024-08-07 09:06:05 +0530

    studentstats

--- project/app/urls.py
+++ project/app/urls.py
@@ -5,5 +5,7 @@ urlpatterns=[
     path("func2",views.function2),
     path("func3/<a>",views.function3),
     path("func4/<int:b>",views.function4),
-    path("func5/<c>",views.function5)
+    path("func5/<c>",views.function5),
+    path("html1",views.html1),
+    path("html2",views.html2)
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def function1(req):
     return HttpResponse('welcome')
@@ -13,4 +14,14 @@ def function3(req,a):
 def function4(req,b):
     return HttpResponse(b)
 def function5(req,c):
-    return HttpResponse(c)    
+    return HttpResponse(c) 
+
+def html1(req):
+    return render(req,'index.html')
+
+def html2(req):
+    a="HELLO"
+    p="you okay"
+    s=[10,20,30]
+    student=[{"name":"arun","age":24,},{"name":"unni","age":22},{"name":"sinu","age":23},{"name":"josep","age":19},{"name":"kannan","age":20}]
+    return render(req,'inox.html',{'data':a,'roll':p,'num':s,'student':student})
