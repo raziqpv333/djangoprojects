3451f111cd4fdff86f7445e2ac36147969e033dc
raziqpv333 <raziqpv333@gmail.com>
2024-08-22 09:05:38 +0530

    edit and delet

--- employeemgt/emplo/urls.py
+++ employeemgt/emplo/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns=[
      path("emplo",views.emplo),
      path("add",views.addemployee),
-     path("dis",views.display)
+     path("dis",views.display),
+     path("edit/<id>",views.edit),
+     path("delete/<id>",views.delete)
 ]
--- employeemgt/emplo/views.py
+++ employeemgt/emplo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from . models import *
 # Create your views here.
@@ -19,3 +19,22 @@ def addemployee(req):
 def display (req):
     data1=employee.objects.all()
     return render (req,"emplo1.html",{"data11":data1})
+
+def edit (req,id):
+    a=employee.objects.get(pk=id)
+    if req.method=='POST':
+        name=req.POST['name']
+        age=int(req.POST['age'])
+        salary=int(req.POST['salary'])
+        jod=req.POST['job']
+        employee.objects.filter (pk=id).update(name=name,age=age,jod=jod,salary=salary)
+        return redirect(display)
+
+
+
+    return render (req,"editemplo.html",{'data':a})
+
+def delete(red,id):
+    a=employee.objects.get(pk=id)
+    a.delete()
+    return redirect(display)
